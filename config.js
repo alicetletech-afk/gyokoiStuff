d9edcdeed27f7428de7eabfef374573c1a76793a
alicetletech-afk <kaitlynn.ont@gmail.com>
2026-07-12 03:50:19 +0700

    Remove temporary password from config.js
    Removed temporary password for security reasons.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.GYOKOI_ADMIN_CONFIG = {
   // TEMPORARY: เปลี่ยนรหัสตรงนี้ก่อนอัป GitHub
   // หมายเหตุ: GitHub Pages เป็นเว็บสาธารณะ รหัสในไฟล์นี้จึงไม่ปลอดภัยจริง
   // เมื่อต่อ Google Apps Script ให้ย้ายการตรวจรหัสไปฝั่ง Apps Script
-  TEMP_PASSWORD: "gyokoi1999!",
+  TEMP_PASSWORD: "",
 
   // ใส่ Apps Script Web App URL ภายหลัง
   API_URL: "https://script.google.com/macros/s/AKfycbwLRLlorkcoL7Znrwv7v799RpJrhAju_4K2eixfFNc9ezcxnzf059GnsGnwI4sa8Kx3dQ/exec"

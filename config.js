db2aa1741b7d2344f8f1af069292ec3c9f54695f
alicetletech-afk <kaitlynn.ont@gmail.com>
2026-07-12 03:42:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.GYOKOI_ADMIN_CONFIG = {
   // TEMPORARY: เปลี่ยนรหัสตรงนี้ก่อนอัป GitHub
   // หมายเหตุ: GitHub Pages เป็นเว็บสาธารณะ รหัสในไฟล์นี้จึงไม่ปลอดภัยจริง
   // เมื่อต่อ Google Apps Script ให้ย้ายการตรวจรหัสไปฝั่ง Apps Script
-  TEMP_PASSWORD: "change-me-gyokoi",
+  TEMP_PASSWORD: "gyokoi1999!",
 
   // ใส่ Apps Script Web App URL ภายหลัง
   API_URL: ""
